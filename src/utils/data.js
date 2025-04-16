83ea4b716ac829d200691cca2a3e48de4f8e4ec3
toshan kanwar <toshankanwar2003@gmail.com>
2025-04-16 22:01:45 +0530

    Initial commit or latest updates

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,31 +1,31 @@
 export const personalInfo = {
-    name: "Dharmendra Dhruw",
-    title: "Full Stack Web Developer | DSA & CP Enthusiast | Electronics Engineer ",
-    email: "ddhruw3337@gmail.com",
-    phone: "+91 *******848",
+    name: "Toshan kanwar",
+    title: "Full Stack Web Developer | ML and AI Enthusiam | Data Engineer ",
+    email: "contact@toshankanwar.website",
+    phone: "+91 *******493",
     location: "International Institute of Information Technology, Naya Raipur, C.G., India",
-    bio: "Versatile Full Stack Web Developer with a background in Electronics Engineering and a strong foundation in Data Structures and Algorithms. Passionate about building efficient, scalable web applications and solving complex problems through competitive programming."
+    bio: "Versatile Full Stack Web Developer with a background in Data Science and AI and a strong foundation in Machine Learning and Prompt Engineering. Passionate about building efficient, scalable web applications and Dive into large data related problems."
   };
   
   export const socialLinks = [
     {
       name: "LinkedIn",
-      url: "https://www.linkedin.com/in/dharmendra-dhruw-7a7291290/",
+      url: "https://www.linkedin.com/in/toshankanwar2005",
       icon: "fab fa-linkedin"
     },
     {
       name: "GitHub",
-      url: "https://github.com/dharmendra23101",
+      url: "https://github.com/toshankanwar",
       icon: "fab fa-github"
     },
     {
       name: "LeetCode",
-      url: "https://leetcode.com/u/ddhruw3337/",
+      url: "https://leetcode.com/u/UncountedBug/",
       icon: "fas fa-code"
     },
     {
-      name: "CodeChef",
-      url: "https://www.codechef.com/users/ddhruw3337",
+      name: "Codeforces",
+      url: "https://codeforces.com/profile/DMT_Toshan",
       icon: "fas fa-terminal"
     }
   ];
@@ -33,90 +33,86 @@ export const personalInfo = {
   export const skills = [
     {
       category: "Programming Languages",
-      items: ["C/C++ (for CP)", "MATLAB", "Python (for ML)", "JavaScript"]
+      items: ["C/C++ (for CP)",  "Python (for ML)", "JavaScript"]
     },
     {
       category: "Web Development",
-      items: ["HTML", "CSS", "React.js", "Node.js", "Express.js", "JavaScript" , "MongoDB", "Firebase"]
+      items: ["HTML", "CSS", "React.js", "Node.js", "Express.js", "JavaScript" , "MongoDB", "Firebase", "SQL"]
     },
     {
       category: "Technical Skills",
-      items: ["DSA/CP", "IoT", "VLSI", "Full Stack Development"]
+      items: ["DSA/CP", "Data Science", "Machine Learning", "Full Stack Development"]
     },
     {
       category: "Hardware and Software Tools",
-      items: ["Arduino", "Raspberry Pi", "OrCAD", "Git", "VS Code", "Jupyter Notebook"]
+      items: ["Git", "VS Code", "Jupyter Notebook","Github","Arduino", "Raspberry Pi" ]
     },
   ];
   
   export const projects = [
     {
-        title: "EasyRide",
-        category: "software",
-        description: "Developed a real-time vehicle booking and rental website to simplify the process of finding and renting vehicles.",
-        image: "project-ppg.jpg",
-        technologies: ["Vite","react","node.js","CSS","HTML","git","Firebase"],
-        github: "https://github.com/dharmendra23101/Easyride",
-        demo: "https://dharmendra23101.github.io/Easyride/"
+        title: "Bakery Ecommerce website",
+        category: "development",
+        description: "Developed a bakery website which shows the working of ecommerce. all functionalities like login signup cart shop are implemented .",
+        image: "bakery.jpg",
+        technologies: ["react","node.js","CSS","HTML","git","Firebase"],
+        github: "https://github.com/toshankanwar/Bakery-Management-System",
+        demo: "https://bakery.toshankanwar.website/"
       },
     {
-      title: "BRT Bus Service",
-      category: "software",
-      description: "Developed a real-time bus tracking system with schedules and fare details.",
-      image: "project-brt.jpg",
-      technologies: ["HTML", "CSS", "JavaScript", "node.js"],
-      github: "https://github.com/Mukund934/BRTS-BusTracking",
-      demo: "https://github.com/Mukund934/BRTS-BusTracking"
+      title: "Heart Failure Prediction",
+      category: "ml",
+      description: "Developed a heart failure prediction web app using deep learning and flask as a backend. It gives the percentage of risk of heart failure",
+      image: "heart.png",
+      technologies: ["React js", "Tailwind CSS", "Flask", "Google Firebase"],
+      github: "https://github.com/toshankanwar/Heart-Failure-Prediction",
+      demo: "https://heart-failure.toshankanwar.website"
     },
     {
-      title: "Weather Pro",
-      category: "software",
-      description: "Weather application providing current and forecast weather data.",
-      image: "project-weather.jpg",
-      technologies: ["HTML", "CSS", "JavaScript", "API"],
-      github: "https://github.com/dharmendra23101/Weather_pro",
-      demo: "https://github.com/dharmendra23101/Weather_pro"
+      title: "AQI Prediction",
+      category: "ml",
+      description: "Give city name and date and here you see magic .",
+      image: "aqi.png",
+      technologies: ["React js", "CSS3", "JavaScript", "Python Flask"],
+      github: "https://github.com/toshankanwar/AQI-Prediction",
+      demo: "https://aqi.toshankanwar.website/"
     },
     {
-      title: "Sudoku Solver",
-      category: "software",
-      description: "Interactive Sudoku puzzle solver with backtracking algorithm.",
-      image: "project-sudoku.jpg",
-      technologies: ["HTML", "CSS", "JavaScript"],
+      title: "Personal Portfolio",
+      category: "development",
+      description: "Personal Portfolio website to showcasing skills and projects",
+      image: "portfolio.png",
+      technologies: ["React", "Vite", "JavaScript"],
       github: "https://dharmendra23101.github.io/sudoku/",
       demo: "https://dharmendra23101.github.io/sudoku/"
     },
     {
-        title: "Portfolio Website",
-        category: "software",
-        description: "Personal portfolio website showcasing skills and projects.",
-        image: "project-portfolio.jpg",
-        technologies: ["HTML", "CSS", "JavaScript", "Vite", "react"],
-        github: "https://dharmendra23101.github.io/Portfolio-project/",
-        demo: "https://dharmendra23101.github.io/Portfolio-project/"
+        title: "Credit card fraud detection",
+        category: "ml",
+        description: "A fraud detection project to detect frauds",
+        image: "credit.webp",
+        technologies: ["Python"],
+        github: "https://github.com/toshankanwar/Advanced-Credit-Card-Fraud-Detection-using-Time-Series-Analysis",
+        demo: "https://github.com/toshankanwar/Advanced-Credit-Card-Fraud-Detection-using-Time-Series-Analysis"
       },
     {
-      title: "IoT-Based Smart Farm Monitoring System",
-      category: "hardware",
-      description: "Implemented an IoT system to monitor farm conditions, improving efficiency.",
-      image: "project-iot-farm.jpg",
-      technologies: ["NodeMCU", "Arduino IDE", "DHT11","Moiture sensor","Motion sensor","Blynk App"]
+      title: "Text Analyzer Tool",
+      category: "development",
+      description: "A basic to to analyze text.",
+      image: "text.jpg",
+      technologies: ["Reactjs","javascript"],
+      github: "https://github.com/toshankanwar/text-analyzer",
+      demo: "https://text.toshankanwar.website/"
+
     },
-    {
-      title: "PPG Signal Feature Extraction",
-      category: "hardware",
-      description: "Developed a system for BP estimation using PPG signals and ML.",
-      image: "project-ppg.jpg",
-      technologies: [ "MAX30100", "ML","NodeMCU", "Signal Processing"]
-    }
   ];
   
   export const education = [
     {
       institution: "International Institute of Information Technology, Naya Raipur",
       degree: "Bachelor of Technology (B.Tech)",
-      field: "Electronics and Communication Engineering",
-      period: "2023 - 2027"
+      field: "Data Science and Artificial Intelligence",
+      period: "2022 - 2026"
     },
     {
       institution: "Jawahar Navodaya Vidyalaya, Kurud Dhamtari, CG",
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Portfolio/', // <-- repo name here (case-sensitive)
+  plugins: [react()], // <-- repo name here (case-sensitive)
 })
