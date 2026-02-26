69ac0068b977a510ebe848631b2740df05153906
toshankanwar <toshankanwar03@gmail.com>
2026-02-26 21:02:40 +0530

    now static assets serve through cdn

--- src/utils/data.js
+++ src/utils/data.js
@@ -118,7 +118,7 @@ export const projects = [
     category: "development",
     description:
       "Bakery Delivery App is a sleek, easy-to-use mobile app built with Expo and React Native, to delivered the bakery item and made for delivery boys. easy to use dark and light theme provided.",
-    image: "delivery-app.jpg",
+    image: "https://static-assets.toshankanwar.in/images/delivery-app.jpg",
     technologies: ["React Native","Expo dev", "UI/UX", "Node.js", "Google Firebase" ],
     github: "https://github.com/toshankanwar/Delivery-Andoid-APP-React-Native-",
     demo: "https://github.com/toshankanwar/cdn-toshan/blob/main/apk/bakery-delivery-app.apk",
@@ -128,7 +128,7 @@ export const projects = [
     category: "development",
     description:
       "Built a full-stack, mobile-first bakery e-commerce website with a seamless app-like experience, enabling customers to browse products, add to cart, checkout, and track orders.",
-    image: "bakery.png",
+    image: "https://static-assets.toshankanwar.in/images/bakery.png",
     technologies: ["Next.js", "Google Firebase", "Tailwind CSS", "DBMS", "Data Analysis", "UI/UX" ],
     github: "https://github.com/toshankanwar/bakery-management-and-ecommerce",
     demo: "https://bakery.toshankanwar.in/",
@@ -138,7 +138,7 @@ export const projects = [
     category: "development",
     description:
       "Developed a robust admin dashboard with advanced role-based access control for secure management of products, orders, and users.",
-    image: "bakery-dashboard.png",
+    image: "https://static-assets.toshankanwar.in/images/bakery-dashboard.png",
     technologies: ["Next.js", "Google Firebase", "Tailwind CSS", "Data Analysis"],
     github: "https://github.com/toshankanwar/Admin-Bakery-Management-and--Ecommerce",
     demo: "https://admin.bakery.toshankanwar.in/login",
@@ -148,7 +148,7 @@ export const projects = [
     category: "development",
     description:
       "Developed a Personal Music listening platform to share and play personal music.",
-    image: "music-app.jpg",
+    image: "https://static-assets.toshankanwar.in/images/music-app.jpg",
     technologies: ["Next.js", "Tailwind CSS", "jsdelivr cdn"],
     github: "https://github.com/toshankanwar/Toshan-Music",
     demo: "https://music.toshankanwar.in",
@@ -158,17 +158,28 @@ export const projects = [
     category: "ml",
     description:
       "Developed a robust resume ranking system based on 11 powerfull algorithms",
-    image: "resume-ranker.png",
+    image: "https://static-assets.toshankanwar.in/images/resume-ranker.png",
     technologies: ["Next.js", "Python Flask", "Tailwind CSS", "ML and NlP"],
     github: "https://github.com/toshankanwar/Resume-Ranking-System",
     demo: "https://resume.toshankanwar.in",
   },
   {
+    title: "Indian Market Bubbles",
+    category: "development",
+    description:
+      "Developed an indian market stock analyzer through bubble vizualizations",
+    image: "https://static-assets.toshankanwar.in/images/indian-bubble.png",
+    technologies: ["React.js", "D3.js", "Tailwind CSS",],
+    github: "https://github.com/toshankanwar/Indian-Market-Bubbles",
+    demo: "https://indian.bubbles.toshankanwar.in/",
+  },
+
+  {
     title: "ScriptPunk Terminal",
     category: "development",
     description:
       "A cutting-edge terminal emulator built with React + TypeScript frontend and Go backend. Features 50+ interactive commands. ",
-    image: "Terminal.png",
+    image: "https://static-assets.toshankanwar.in/images/Terminal.png",
     technologies: ["React.js","Vite.js","Go","xterm.js" ],
     github: "https://github.com/toshankanwar/Project-X",
     demo: "https://terminal.toshankanwar.in",
@@ -178,7 +189,7 @@ export const projects = [
     category: "development",
     description:
       "A real-time chat application built with React Native, Expo, and Firebase featuring WhatsApp-like messaging experience with offline support. ",
-    image: "securelink.png",
+    image: "https://static-assets.toshankanwar.in/images/securelink.png",
     technologies: ["React Native","App development","Expo dev","Google Firebase","Node.js"],
     github: "https://github.com/toshankanwar/SecureLink-V2",
     demo: "https://github.com/toshankanwar/cdn-toshan/blob/main/apk/SecureLink.apk",
@@ -188,7 +199,7 @@ export const projects = [
     category: "development",
     description:
       "Stock Broking is a modern mobile Stock Broking application that provides top gainers and losers insights. Built with React Native and Expo. ",
-    image: "Stock-Broking.png",
+    image: "https://static-assets.toshankanwar.in/images/Stock-Broking.png",
     technologies: ["React Native","App development","Expo dev"],
     github: "https://github.com/toshankanwar/Stock-Etf-Broking-Groww",
     demo: "https://github.com/toshankanwar/cdn-toshan/blob/main/apk/Stock-Broking.apk",
@@ -199,7 +210,7 @@ export const projects = [
     category: "development",
     description:
       "Developed a fully responsive poem-sharing platform using Next.js 13+ App Router architecture and Firebase backend. ",
-    image: "poemsite.png",
+    image: "https://static-assets.toshankanwar.in/images/poemsite.png",
     technologies: ["Next.js","Node.js","Nodemailer"," Google Firebase", "Tailwind CSS", "Zoho mail service" ],
     github: "https://github.com/toshankanwar/poetry-website",
     demo: "https://poems.toshankanwar.in",
@@ -209,7 +220,7 @@ export const projects = [
     category: "development",
     description:
       "Powerfull admin dashboard to add delete update poems ,see and reply to comments ,process request from other poets ",
-    image: "poemsite-admin.png",
+    image: "https://static-assets.toshankanwar.in/images/poemsite-admin.png",
     technologies: ["Next.js","Node.js","Nodemailer"," Google Firebase", "Tailwind CSS" ],
     github: "https://github.com/toshankanwar/admin-poetry-website",
     demo: "https://admin.poems.toshankanwar.in",
@@ -221,7 +232,7 @@ export const projects = [
     category: "ml",
     description:
       "Developed a heart failure prediction web app using deep learning and flask as a backend. It gives the percentage of risk of heart failure",
-    image: "heart.png",
+    image: "https://static-assets.toshankanwar.in/images/heart.png",
     technologies: ["React.js", "Tailwind CSS", "Python Flask", "Google Firebase"],
     github: "https://github.com/toshankanwar/Heart-failure-Prediction-final",
     demo: "https://heart.toshankanwar.in",
@@ -230,7 +241,7 @@ export const projects = [
     title: "Text Analyzer Tool",
     category: "development",
     description: "A web app to analyze, transform, and translate text instantly—featuring document upload, keyword highlighting, English/Hindi translation, and speech synthesis.",
-    image: "text.png",
+    image: "https://static-assets.toshankanwar.in/images/text.png",
     technologies: ["React.js", "Vite.js", "pdfjs-dist","mammoth"],
     github: "https://github.com/toshankanwar/Advance-Text-Analyzer",
     demo: "https://text.toshankanwar.in/",
@@ -239,7 +250,7 @@ export const projects = [
     title: "Personal Portfolio",
     category: "development",
     description: "Personal Portfolio website to showcasing skills and projects",
-    image: "portfolio.png",
+    image: "https://static-assets.toshankanwar.in/images/portfolio.png",
     technologies: ["React.js", "Vite.js", "JavaScript"],
     github: "https://github.com/toshankanwar/portfolio-f",
     demo: "https://toshankanwar.in/",
@@ -249,7 +260,7 @@ export const projects = [
     category: "ml",
     description:
       "Currently in Development phase/updated mode .",
-    image: "crypto.png",
+    image: "https://static-assets.toshankanwar.in/images/crypto.png",
     technologies: ["React.js", "Python Flask", "CSS3"],
     github: "https://github.com/toshankanwar/Stock-Market-Prediction-Web-App",
     demo: "https://stock-market-prediction-web-app.vercel.app/",
@@ -258,7 +269,7 @@ export const projects = [
     title: "AQI Prediction",
     category: "ml",
     description: "Give city name and date and here you see magic. But as their are 250+ models for individual cities unfortunately i cant host backend.",
-    image: "aqi.png",
+    image: "https://static-assets.toshankanwar.in/images/aqi.png",
     technologies: ["React.js","Python Flask", "CSS3"],
     github: "https://github.com/toshankanwar/AQI-Prediction",
     demo: "https://aqi.toshankanwar.in/",
@@ -268,7 +279,7 @@ export const projects = [
     title: "Credit card fraud detection",
     category: "ml",
     description: "A fraud detection project to detect frauds",
-    image: "credit.webp",
+    image: "https://static-assets.toshankanwar.in/images/credit.webp",
     technologies: ["Python"],
     github:
       "https://github.com/toshankanwar/Advanced-Credit-Card-Fraud-Detection-using-Time-Series-Analysis",
